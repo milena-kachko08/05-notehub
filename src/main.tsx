e233fb60f6bc0fcf7cb4af9df6009a21d6c48cd1
Milena Kachko <kachkomilena@gmail.com>
2025-08-27 20:23:03 +0200

    add hw

--- src/main.tsx
+++ src/main.tsx
@@ -9,7 +9,7 @@ const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
+    <QueryClientProvider client={queryClient} >
       <App />
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
