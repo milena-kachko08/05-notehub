344f07ac0ecbe34ef1585b7bfac06785df48a207
Milena Kachko <kachkomilena@gmail.com>
2025-09-07 21:25:01 +0200

    add

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App/App";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import "modern-normalize/modern-normalize.css";
+import 'modern-normalize/modern-normalize.css';
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 const queryClient = new QueryClient();
